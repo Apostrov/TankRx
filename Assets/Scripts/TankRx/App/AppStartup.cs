2ab012cea00ac48fac94c31d232a28a4ff4bcdae
Apostrov <amir.subaev@gmail.com>
2023-03-07 15:08:01 +0300

    Base scene and input system setup

---
+++ Assets/Scripts/TankRx/App/AppStartup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace TankRx.App
+{
+    public class AppStartup : MonoBehaviour
+    {
+        private bool _loadStarted = false;
+
+        private void LoadGame()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+
+        void Start()
+        {
+            _loadStarted = false;
+            InitCallback();
+        }
+
+        private void InitCallback()
+        {
+            if (_loadStarted)
+                return;
+
+#if UNITY_EDITOR
+            LoadGame();
+#else
+            Invoke(nameof(LoadGame), 0.89f);
+#endif
+
+            _loadStarted = true;
+        }
+    }
+}
